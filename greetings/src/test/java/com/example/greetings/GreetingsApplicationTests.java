01a899b6024e98d167155875a3a0f0dbf33d6b1b
Nidhi Gupta <56179878+NidhxGupta@users.noreply.github.com>
2022-04-20 13:23:29 +0530

    Update

--- greetings/src/test/java/com/example/greetings/GreetingsApplicationTests.java
+++ greetings/src/test/java/com/example/greetings/GreetingsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.music;
+package com.example.greetings;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MusicApplicationTests {
+class GreetingsApplicationTests {
 
 	@Test
 	void contextLoads() {
